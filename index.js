11db0cb054bfa4116da415ad05eeade838ed3cb8
dariuszdroba <dariuszdroba@gmail.com>
2024-07-05 17:44:59 +0300

    health-checker

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const health = require('@cloudnative/health-connect');
+let healthcheck = new health.HealthChecker();
+
 const app = express();
 
 const basicRouter = require('./routes/basicRoutes');
@@ -7,9 +10,11 @@ const basicRouter = require('./routes/basicRoutes');
 app.get('/', (req,res) => {
     return res.send("<h2>Express demo -docker-link-test- </h2>");
 })
-
+app.use('/live', health.LivenessEndpoint(healthcheck))
+app.use('/ready', health.ReadinessEndpoint(healthcheck))
+app.use('/health', health.HealthEndpoint(healthcheck))
 app.use(basicRouter)
 
-app.listen(4141, () => {
-    console.log("listening on port 4141");
+app.listen(3000, () => {
+    console.log("listening on port 3000");
 })
